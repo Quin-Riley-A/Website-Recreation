a08aed59d6128b1182a333547011c25c41b1c2b0
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-28 14:33:35 -0400

    begin file and test append co-authored-by: Rafael Petrachini ​<raffp@me.com>

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  const body = document.querySelector("body");
+  const p1 = document.createElement("p");
+  p1.append("The HTML of this webpage was created with JavaScript");
+  body.append(p1);
+}
